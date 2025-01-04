d6c666118918d42a02197ba219da5c7ef178ea0d
Xrtion <kicaubae@gmail.com>
2025-01-04 18:21:45 +0700

    Login Screen

---
+++ blinkit_clone/lib/repository/screens/splash/splahscreen.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+import 'package:blinkit_clone/domain/constants/appcolors.dart';
+import 'package:blinkit_clone/repository/screens/logins/loginscreen.dart';
+import 'package:blinkit_clone/repository/widgets/uihelper.dart';
+import 'package:flutter/material.dart';
+
+class Splahscreen extends StatefulWidget {
+  @override
+  State<Splahscreen> createState() => _SplahscreenState();
+}
+
+class _SplahscreenState extends State<Splahscreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => Loginscreen()),
+      );
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Appcolors.scaffoldbackground,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Uihelper.CustomImage(img: "image 1.png"),
+          ],
+        ),
+      ),
+    );
+  }
+}
